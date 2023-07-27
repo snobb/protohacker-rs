bdff1bf775d0db0596cb435506f27da3e829c0b2
Alex Kozadaev <akozadaev at yahoo com>
2023-07-27 20:38:03 +0100

    Simplifying tcpserver yet again

--- 00-smoke-test/src/main.rs
+++ 00-smoke-test/src/main.rs
@@ -1,10 +1,31 @@
-mod echo;
-
 use protohacker_lib::config::Config;
 use protohacker_lib::tcpserver::TcpServer;
-use std::io;
+use std::io::{self, Read, Write};
+use std::net::TcpStream;
+
+pub fn handle(addr: &str, mut stream: Box<TcpStream>) {
+    let mut buf = [0; 2048];
+
+    println!("Handling request from {addr}");
+
+    loop {
+        match stream.read(&mut buf) {
+            Ok(n) => {
+                if n == 0 {
+                    break;
+                }
+
+                _ = stream.write(&buf[0..n]).unwrap();
+            }
+            Err(error) => {
+                println!("Error: {error}");
+                return;
+            }
+        }
+    }
+}
 
 fn main() -> io::Result<()> {
     let config = Config::load();
-    TcpServer::new(config.addr).listen(echo::handle)
+    TcpServer::new(config.addr).listen(handle)
 }
